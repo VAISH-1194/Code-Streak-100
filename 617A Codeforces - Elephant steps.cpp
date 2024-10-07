f0e564fbaee51574a6cee24444adebf5efb260c1
VAISHNAVI M <125114509+VAISH-1194@users.noreply.github.com>
2024-10-07 13:40:54 +0530

    Update 617A Codeforces - Elephant steps.cpp

--- 617A Codeforces - Elephant steps.cpp
+++ 617A Codeforces - Elephant steps.cpp
@@ -1,26 +1,46 @@
-// Method 1
+##QUESTION:
 
-// #include <iostream>
-// using namespace std;
+An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's 
+house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 
+positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
 
-// int main() {
-//     int n, ans = 0;
-//     int a[] = {5, 4, 3, 2, 1};
+Input
+The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
 
-//     cin >> n;
+Output
+Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
 
-//     for (int i = 0; i < 5; i++) {
-//         ans += n / a[i];
-//         n = n % a[i]; 
-//     }
+Examples
+InputCopy
+5
+OutputCopy
+1
 
-//     cout << ans << endl; 
-//     return 0;
-// }
+##ANSWER:
+
+##Method 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, ans = 0;
+    int a[] = {5, 4, 3, 2, 1};
+
+    cin >> n;
+
+    for (int i = 0; i < 5; i++) {
+        ans += n / a[i];
+        n = n % a[i]; 
+    }
+
+    cout << ans << endl; 
+    return 0;
+}
 
 
 
-// Alias Method
+##Alias Method
 
 #include <iostream>
 using namespace std;
@@ -33,10 +53,10 @@ int main() {
 }
 
 
-// Calculation for alias method
+##Calculation for alias method
 
-// 11 -> 3
-// 11 + 4 = 15 / 5 => 3
+11 -> 3
+11 + 4 = 15 / 5 => 3
 
-// 14 -> 3
-// 14 + 4 = 18 / 5 => 3
+14 -> 3
+14 + 4 = 18 / 5 => 3
