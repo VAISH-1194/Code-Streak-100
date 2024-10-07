a5a6923cfd79a0a5100e282d9c8aac938e9a0eeb
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-07 16:09:08 +0530

    Bubble sort

---
+++ Day 1/Sorting an Array using Pointers - Bubble Sort.cpp
@@ -0,0 +1,44 @@
+// Sorting an Array using Pointers (Bubble Sort)
+
+
+#include <iostream>
+using namespace std;
+
+void sort(int *arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (*(arr + j) > *(arr + j + 1))
+            {
+                swap(*(arr + j), *(arr + j + 1));
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
