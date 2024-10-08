a24ce5dba084da59e55e8ddc993d4cf6a359fdc1
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-08 14:22:43 +0530

    sum of first n square natural numbers

---
+++ Day 2/Sum of square of first N elements.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+       Scanner sc = new Scanner(System.in);
+       
+       int n = sc.nextInt();
+       int res = 0;
+       
+       for(int i=1;i<=n;i++)
+       {
+            res += i*i;
+       }
+       
+       System.out.println(res);
+       
+    }
+}
