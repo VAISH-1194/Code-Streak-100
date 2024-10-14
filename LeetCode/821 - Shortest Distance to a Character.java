dc50ac625e05ff5b5a160a00115fee518e298ff6
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-14 12:12:22 +0530

    updated optimised method 2

--- LeetCode/821 - Shortest Distance to a Character.java
+++ LeetCode/821 - Shortest Distance to a Character.java
@@ -1,3 +1,11 @@
+Question 
+
+https://leetcode.com/problems/shortest-distance-to-a-character/
+
+
+// Method 1
+
+
 class Solution {
     public int[] shortestToChar(String s, char c) {
           int n=s.length();
@@ -14,5 +22,40 @@ class Solution {
             arr[i]=min;
             }
             return arr;
+    }
+}
+
+
+// Optimised Method 2
+
+
+class Solution
+{
+    public int[] shortestToChar(String s, char c)
+    {
+        int n = s.length();
+        int res[] = new int[n];
+        int k=0, last = 10000000;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i) == c)
+            {
+                res[k++] = 0;
+                last = i;
+            }
+            else
+            {
+                int j = i;
+                while(j<n && s.charAt(j) != c)
+                    j++;
+                if(j<n)
+                    res[k++] = Math.min(Math.abs(i-j),Math.abs(i-last));
+                else
+                    res[k++] = Math.abs(i-last);
+            }
+        }
+
+        return res;
     }
 }
