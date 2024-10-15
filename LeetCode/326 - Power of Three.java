f634812ce1fccb50f1f1186aac549e6b0001014a
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-15 11:57:56 +0530

    using recursion

---
+++ LeetCode/326 - Power of Three.java
@@ -0,0 +1,14 @@
+Question
+https://leetcode.com/problems/power-of-three/
+
+Answer
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+       if(n<=0)
+            return false;
+        if(n==1)
+            return true;
+        return (n%3 == 0 && isPowerOfThree(n/3));
+    }
+}
