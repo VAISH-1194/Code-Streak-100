d1ecf31d0af6e8a001219aacde1be83df02acf93
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-15 14:40:05 +0530

    recursion

---
+++ Day 5/223339B Codeforces - Print From 1 to N.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main 
+{
+  public static void print(int n)
+  {
+    if(n<=0)
+      return;
+    print(n-1); 
+    System.out.println(n);
+  }
+  
+  public static void main(String[] args) 
+  {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      
+      print(n);
+  }
+}
