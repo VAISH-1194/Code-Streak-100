2d46369a8c774190a9edb096cb8d627113334e1e
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-15 14:40:19 +0530

    recursion

---
+++ Day 5/223339A Codeforces - Print Recursion.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main 
+{
+  public static void print(int n)
+  {
+    if(n<=0)
+      return;
+    System.out.println("I love Recursion");
+    print(n-1); 
+  }
+  
+  public static void main(String[] args) 
+  {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      
+      print(n);
+  }
+}
