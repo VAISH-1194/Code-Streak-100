c6256cb0239661294275725604e37c8dec424d58
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-15 10:22:49 +0530

    sum of n natural numbers

---
+++ Day 5/Linear Recursion.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  
+  public static int sum(int n)
+  {
+      if(n<=0) return 0;
+      return n+sum(n-1);
+  }
+    
+  public static void main(String[] args) 
+  {
+     System.out.println(sum(10));
+  }
+}
