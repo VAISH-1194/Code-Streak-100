63e2ffb454dc6b50ee6215cd41756314e1a2dcdd
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-16 09:48:51 +0530

    collections

---
+++ Day 6/Collection Syntax.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+     Collection<Integer> list = new ArrayList<Integer>();
+     list.add(11);
+     list.add(9);
+     list.add(2004);
+     
+     System.out.println(list);
+  }
+}
