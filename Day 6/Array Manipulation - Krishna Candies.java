c683f1175a38e3a1507e8f81b686df31de8d53a2
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-16 15:13:06 +0530

    Array Manipulation

---
+++ Day 6/Array Manipulation - Krishna Candies.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+      int t = sc.nextInt();
+      
+      while(t-- != 0)
+      {
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i=0;i<n;i++)
+          arr[i] = sc.nextInt();
+          
+        int totalTime = 0;
+        int newBox = arr[0];
+        
+        for(int i=1; i<n;i++)
+        {
+          newBox += arr[i];
+          totalTime += newBox;
+        }
+        
+        System.out.println(totalTime);
+      }
+      
+  }
+}
