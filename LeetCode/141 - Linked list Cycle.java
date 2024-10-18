a80cabc1768fa273668eda7f6a5cc99ead3ae827
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2024-10-18 11:50:42 +0530

    leet 141

---
+++ LeetCode/141 - Linked list Cycle.java
@@ -0,0 +1,23 @@
+Question
+
+https://leetcode.com/problems/linked-list-cycle/
+
+Answer
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;        
+    }
+}
